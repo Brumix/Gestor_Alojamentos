04b7e58eeb8ddf59b01b821cdc343fff1f587b4d
Brumix2000 <brunompereira42@gmail.com>
2020-11-18 04:17:40 +0000

    cmake files

---
+++ cmake-build-debug-coverage/CMakeFiles/Gestor_Alojamentos.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Gestor_Alojamentos.dir/code/edificios/edificios.c.obj"
+  "CMakeFiles/Gestor_Alojamentos.dir/code/localizacao/localizacao.c.obj"
+  "CMakeFiles/Gestor_Alojamentos.dir/code/main/main.c.obj"
+  "Gestor_Alojamentos.exe"
+  "Gestor_Alojamentos.exe.manifest"
+  "Gestor_Alojamentos.pdb"
+  "libGestor_Alojamentos.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Gestor_Alojamentos.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
