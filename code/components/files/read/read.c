8283296c7c45f2cd51b64dd5856ada84ee2d0c98
Brumix2000 <brunompereira42@gmail.com>
2021-01-06 01:35:23 +0000

    cmake

--- code/components/files/read/read.c
+++ code/components/files/read/read.c
@@ -92,18 +92,18 @@ void read_file_studio_politics(BUILDINGS *buildings) {
         fscanf(file_read, "%d", &estudio);
         fscanf(file_read, " %*s %[^,]s", politica);
         fscanf(file_read, " %*s ");
-       STUDIOS *indexstudio= find_studio_everyhere(buildings,estudio);
-        ERRORMESSAGE(indexstudio==NULL,"[READ FILE ESTUDIO:ESTUDIO INEXISTENTE]");
+       STUDIOS *studios= find_studio_everyhere(buildings,estudio);
+        ERRORMESSAGE(studios==NULL,"[READ FILE ESTUDIO:ESTUDIO INEXISTENTE]");
 
 
-        printf(" %i estudio=%d index=%i  %s \n",indexstudio->index,estudio,indexstudio->index,politica);
+        printf("estudio=%d index=%i  %s \n",estudio,studios->index,politica);
 
         POLITICS *findPolitics= find_politics(politica);
         ERRORMESSAGE(findPolitics==NULL,"[READ FILE ESTUDIO:POLITICA INEXISTENTE]");
 
-        add_branch_calendar(indexstudio,findPolitics->platform,1,findPolitics->name);
+        add_branch_calendar(studios,findPolitics->platform,1,findPolitics->name);
 
-        CONFIGURATION * pConfiguration=indexstudio->branch_calendar[indexstudio->number_branch -1].configuration;
+        CONFIGURATION * pConfiguration=studios->branch_calendar[studios->number_branch -1].configuration;
 
 
         printf(" Iteracao =%i\n",j);
